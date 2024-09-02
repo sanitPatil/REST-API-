e8881fa7ce922f3a6c175323d084f7354b7f7c2a
sanitPatil <raghavpatilrp5@gmail.com>
2024-09-02 13:38:18 +0530

    express and env setup done

--- server.ts
+++ server.ts
@@ -1,5 +1,12 @@
-function add() {
-  console.log()
+import { config } from './config/config'
+import app from './src/app'
+
+const PORT = config.PORT || 3000
+
+const startServer = () => {
+  app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`)
+  })
 }
 
-add()
+startServer()
