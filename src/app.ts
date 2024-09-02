2be71af0e41d371b3f719f79f7749a3c912ee29a
sanitPatil <raghavpatilrp5@gmail.com>
2024-09-02 15:29:37 +0530

    Error Resolved

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import express from 'express'
 import ApiError from './utils/ApiError'
-import createHttpError from 'http-errors'
+
 const app = express()
 
 app.get('/', (req, res, next) => {
-  const error = createHttpError(400, 'something went wrong')
-  throw error
   res.json({
     message: 'ok',
   })
